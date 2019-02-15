eeacbf394770fc67eb55c193c710215b5b897f70
smitsohu <smitsohu@gmail.com>
2019-02-15 19:27:42 +0100

    misc enhancements

--- C/lfun.c
+++ C/lfun.c
@@ -3,7 +3,7 @@
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
-   in the *Software without restriction, including without limitation the rights
+   in the Software without restriction, including without limitation the rights
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is furnished
    to do so, subject to the following conditions:
@@ -23,10 +23,10 @@
 /*************************************************
 
         program: lfun.c
+        scans *.c and *.cc source files for suspicious strncmp
+        function calls
 
-        use: gcc -o lfunc lfun.c; lfunc <directory>
-        scans all *.c and *.cc source files in a directory for suspicious
-        strncmp function calls
+        use: gcc -o lfunc lfun.c; lfunc <file or directory>
 
         complain, if
               * at least one argument is a string literal and
@@ -91,7 +91,7 @@ int parse_str(const char *path, const unsigned linecnt, const char *fcall) {
 			while (*tmp) {
 				if (*tmp == '"')
 					break;
-				if (*tmp == '\\' && tmp[1] == '"') {
+				if (*tmp == '\\' && tmp[1]) {  // get past the following byte, it could be '"'
 					tmp++;
 					index++;
 				}
@@ -280,20 +280,21 @@ int read_file(const char *path, const struct stat *s, const int typeflag, struct
 // return value 0: boring
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		printf("Usage: %s <directory>\n", argv[0]);
+		printf("usage: %s <file or directory>\n", argv[0]);
 		return 1;
 	}
-	char *dirname = argv[1];
+	char *name = argv[1];
 
 	struct stat s;
-	if (stat(dirname, &s) == -1 || !S_ISDIR(s.st_mode)) {
-		printf("No directory %s, exiting ...\n", dirname);
+	if (stat(name, &s) == -1 || (!S_ISDIR(s.st_mode) && !S_ISREG(s.st_mode))) {
+		printf("no file or directory %s\n", name);
 		return 1;
 	}
+	printf("------------ scanning %s\n", name);
 
-	int rv = nftw(dirname, read_file, 8, 0);
+	int rv = nftw(name, read_file, 8, 0);
 	if (rv == 1) {
-		printf("Walking the directory tree failed, check your permissions ...\n");
+		puts("walking the directory tree failed, check your permissions ...");
 		return 1;
 	}
 	if (rv == -1) {
@@ -302,7 +303,9 @@ int main(int argc, char **argv) {
 	}
 	printf("parsed %u %s calls\n", calls, FUNCTION);
 
-	if (boring)
+	if (boring) {
+		puts("nothing found!");
 		return 0;
+	}
 	return 1;
 }
